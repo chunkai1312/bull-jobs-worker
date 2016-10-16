7976a59f91aa88fbe309dfc82dbe55256f8445c7
Chun-Kai Wang <chunkai1312@gmail.com>
2016-10-17 04:47:53 +0800

    feat: add port

--- config/local.js
+++ config/local.js
@@ -4,5 +4,6 @@ const path = require('path')
 
 module.exports = {
   env: process.env.NODE_ENV || 'development',  // environment variable, default = development
+  port: process.env.PORT || 8080,              // server port
   root: path.normalize(`${__dirname}/..`)      // root path
 }
