292c030e7e767375193797fd0f9208e9349cb0ec
Chun-Kai Wang <chunkai1312@gmail.com>
2017-05-21 00:28:29 +0800

    refactor: config

---
+++ src/config/env.js
@@ -0,0 +1,5 @@
+export default {
+  development: {},
+  test: {},
+  production: {}
+}
--- src/config/local.js
+++ src/config/local.js
@@ -1,9 +1,3 @@
-'use strict'
+export default {
 
-const path = require('path')
-
-module.exports = {
-  env: process.env.NODE_ENV || 'development',  // environment variable, default = development
-  port: process.env.PORT || 8080,              // server port
-  root: path.normalize(`${__dirname}/..`)      // root path
 }
