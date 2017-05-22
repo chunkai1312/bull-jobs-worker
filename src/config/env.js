a62462fb8827253fa0b549cd3758bfe3deb5fff1
Chun-Kai Wang <chunkai1312@gmail.com>
2017-05-22 14:37:27 +0800

    refactor(config): env

--- src/config/env.js
+++
@@ -1,5 +0,0 @@
-export default {
-  development: {},
-  test: {},
-  production: {}
-}
--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,11 @@
 import merge from 'lodash/merge'
 import config from './config'
-import envConfig from './env'
+import development from './env/development'
+import test from './env/test'
+import production from './env/production'
 
-export default merge({}, config, envConfig[process.env.NODE_ENV || 'development'])
+export default {
+  development: merge({}, config, development),
+  test: merge({}, config, test),
+  production: merge({}, config, production)
+}[process.env.NODE_ENV || 'development']
