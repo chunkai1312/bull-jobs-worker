1679b2df06d4be6d9b93b10ba21aca1cef98a27b
Chun-Kai Wang <chunkai1312@gmail.com>
2017-05-23 03:00:11 +0800

    refactor: config

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,4 @@
-import path from 'path'
-
 export default {
-  env: process.env.NODE_ENV || 'development',  // environment variable, default = development
-  port: process.env.PORT || 8080,              // server port
-  root: path.normalize(`${__dirname}/..`),     // root path
-
   redis: {
     port: process.env.REDIS_PORT,
     host: process.env.REDIS_HOST
